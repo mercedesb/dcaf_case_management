c542d6bdb8a0cea8e3a48f6bd6f61285ac092942
Mercedes Bernard <mercedesrbernard@gmail.com>
2024-06-01 13:42:10 -0500

    restyled form using CSS grid

--- app/javascript/src/components/PatientDashboardForm.jsx
+++ app/javascript/src/components/PatientDashboardForm.jsx
@@ -9,93 +9,93 @@ const PatientDashboardForm = ({
   weeksOptions,
   daysOptions,
   initialCallDate,
-  statusTooltip
+  statusTooltip,
+  isAdmin,
+  patientPath
 }) => {
   const i18n = usei18n();
 
+  console.log(`patient_initial_call_date: ${patient.initial_call_date}`);
+
   return (
-    <>
-      <div className="row">
-        <div className="col-4">
-          <Input
-            id="patient_name"
-            name="patient[name]"
-            label={i18n.t('patient.shared.name')}
-            value={patient.name}
-            required
-          />
-        </div>
+    <div className="grid grid-columns-3 grid-rows-2">
+      <Input
+        id="patient_name"
+        name="patient[name]"
+        label={i18n.t('patient.shared.name')}
+        value={patient.name}
+        required
+      />
 
-        <div className="col">
-          <Select
-            id="pateint_last_menstrual_period_weeks"
-            name="patient[last_menstrual_period_weeks"
-            label={i18n.t('patient.dashboard.weeks_along')}
-            options={weeksOptions}
-            value={weeksOptions.find(opt => opt.selected)?.value}
-          />
-        </div>
+      <div className="grid grid-columns-2">
+        <Select
+          id="pateint_last_menstrual_period_weeks"
+          name="patient[last_menstrual_period_weeks"
+          label={i18n.t('patient.dashboard.weeks_along')}
+          options={weeksOptions}
+          value={weeksOptions.find(opt => opt.selected)?.value}
+        />
 
-        <div className="col mt-4">
-          <Select
-            id="pateint_last_menstrual_period_days"
-            name="patient[last_menstrual_period_days"
-            label={i18n.t('common.days_along')}
-            labelClassName="sr-only"
-            options={daysOptions}
-            value={daysOptions.find(opt => opt.selected)?.value}
-            help={i18n.t('patient.dashboard.called_on', { date: initialCallDate })}
-          />
-        </div>
-
-        <div className="col-3">
-          <Input
-            id="patient_appointment_date"
-            name="patient[appointment_date]"
-            label={i18n.t('patient.shared.appt_date')}
-            type="date"
-            help={
-              i18n.t('patient.dashboard.approx_gestation', {
-                weeks: patient.last_menstrual_period_at_appt_weeks,
-                days: patient.last_menstrual_period_at_appt_days
-              })
-            }
-          />
-        </div>
+        <Select
+          id="pateint_last_menstrual_period_days"
+          name="patient[last_menstrual_period_days"
+          label={i18n.t('common.days_along')}
+          labelClassName="sr-only"
+          options={daysOptions}
+          value={daysOptions.find(opt => opt.selected)?.value}
+          help={i18n.t('patient.dashboard.called_on', { date: initialCallDate })}
+        />
       </div>
 
-      <div className="row">
-        <div className="col-4">
-          <Input
-            id="patient_primary_phone"
-            name="patient[primary_phone]"
-            label={i18n.t('patient.dashboard.phone')}
-            value={patient.primary_phone_display}
-          />
-        </div>
+      <Input
+        id="patient_appointment_date"
+        name="patient[appointment_date]"
+        label={i18n.t('patient.shared.appt_date')}
+        type="date"
+        help={
+          i18n.t('patient.dashboard.approx_gestation', {
+            weeks: patient.last_menstrual_period_at_appt_weeks,
+            days: patient.last_menstrual_period_at_appt_days
+          })
+        }
+      />
 
-        <div className="col">
-          <Input
-            id="patient_pronouns"
-            name="patient[pronouns]"
-            label={i18n.t('activerecord.attributes.patient.pronouns')}
-            value={patient.pronouns}
-          />
-        </div>
+      <Input
+        id="patient_primary_phone"
+        name="patient[primary_phone]"
+        label={i18n.t('patient.dashboard.phone')}
+        value={patient.primary_phone_display}
+      />
 
-        <div className="col">
-          <div className="form-group">
-            <Input
-              id="patient_status_display"
-              label={i18n.t('patient.shared.status')}
-              value={patient.status}
-              className="form-control-plaintext"
-              tooltip={statusTooltip}
-            />
-          </div>
-        </div>
+      <div className="grid grid-columns-2">
+        <Input
+          id="patient_pronouns"
+          name="patient[pronouns]"
+          label={i18n.t('activerecord.attributes.patient.pronouns')}
+          value={patient.pronouns}
+        />
+
+        <Input
+          id="patient_status_display"
+          label={i18n.t('patient.shared.status')}
+          value={patient.status}
+          className="form-control-plaintext"
+          tooltip={statusTooltip}
+        />
       </div>
-    </>
+
+
+      <div>
+        {isAdmin && (
+          <>
+            <label>{i18n.t('patient.dashboard.delete_label')}</label>
+            <div>
+              <a className="btn btn-danger" data-confirm={i18n.t('patient.dashboard.confirm_del', { name: patient.name })} rel="nofollow" data-method="delete" href={patientPath}>{i18n.t('patient.dashboard.delete')}</a>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
   )
 };
 
